907c7f228dd15622222b8ad54ceff7f8900674cd
Nchargui <123753543+Nchargui@users.noreply.github.com>
2025-12-14 18:33:53 -0500

    correction pour progression, suppression de plantes, modification des last time watered, modification des details de la plantes

--- src/api/inventoryApi.jsx
+++ src/api/inventoryApi.jsx
@@ -5,4 +5,5 @@ export const InventoryApi = {
     getMyPlants : () => api.get("/inventory/me/plants"),
     searchNamePlant: (name) => api.get(`/inventory/search?name=${name}`),
     filter: (category) => api.get(`/inventory/filter?category=${category}`),
+    deletePlant: (id) => api.delete(`/inventory/me/plants/${id}`),
 };
