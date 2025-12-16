5f73d5020cfdd4e3580a268cc1848b39d4985df8
Andy <Andylamothe22@hotmail.com>
2025-12-15 19:50:07 -0500

    Update API base URL and fix user ID retrieval in ChatbotScreen

--- src/api/chatbotApi.jsx
+++ src/api/chatbotApi.jsx
@@ -1,6 +1,6 @@
 import { api } from "./axiosInstance";
 
 export const ChatbotApi = {
-  saveRecommendation: (data) => api.post("/recommendation", data),
-  getRecommendationsByUserId: (userId) => api.get(`/recommendation/${userId}`),
+  saveRecommendation: (data) => api.post("/recommendations", data),
+  getRecommendationsByUserId: (userId) => api.get(`/recommendations/${userId}`),
 };
